1903d7b5de6d5ef5cb897e39ce6fca999b57b66f
Luccas Silveira <luccas.carsil@gmail.com>
2017-05-24 16:18:48 -0300

    Criação de classe de configurações gerais do site.

---
+++ scripts/config.php
@@ -0,0 +1,13 @@
+<?php
+
+class Config {
+
+	public static function getSmtpConfig() {
+		return array(
+			'host'     => '',
+			'port'     => 587,
+			'username' => '',
+			'password' => ''
+		);
+	}
+}
