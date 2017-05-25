2c0d4266ecae40b0a8dee3ab8787fd6a1e7a722b
Luccas Silveira <luccas.carsil@gmail.com>
2017-05-25 19:23:19 -0300

    Correção da responsividade de página de erro 404.

--- views/errors/404.php
+++ views/errors/404.php
@@ -4,7 +4,7 @@
 		<title>Luccas Silveira - Página não encontrada</title>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 		<meta name="viewport" content="width=device-width, height=device-height, initial-scale=1, maximum-scale=3"/>
-		<link href="../assets/images/favicon.png" type="image/x-icon" rel="shortcut icon"/>
+		<link href="<?php echo BASE_URL; ?>/assets/images/favicon.png" type="image/x-icon" rel="shortcut icon"/>
 		<script>
 			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
@@ -14,16 +14,15 @@
 			ga('create', 'UA-74371832-1', 'auto');
 			ga('send', 'pageview');
 		</script>
-		<link rel="stylesheet" type="text/css" href="../assets/css/frontend.css"/>
+		<link rel="stylesheet" type="text/css" href="<?php echo BASE_URL; ?>/assets/css/frontend.css"/>
 	</head>
 	<body id="error-page-content">
-		<?php session_start(); ?>
-		<header class="row section-wrapper">
-			<div id="error-message" class="column column-12">
+		<header class="row">
+			<div id="error-message" class="section-wrapper column column-12">
 				<h1>404</h1>
 				<h2>Página não encontrada!</h2>
 				<p>
-					Voltar para a <a href="<?php echo $_SESSION['BASE_URL']; ?>">página inicial</a>.
+					Voltar para a <a href="<?php echo BASE_URL; ?>">página inicial</a>.
 				</p>
 			</div>
 		</header>
