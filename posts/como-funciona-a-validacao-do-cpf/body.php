ccba5eb8e0196fdca1adc5d0cc0733fa42cae3a9
lu2cas <luccas.carsil@gmail.com>
2017-06-06 21:56:44 -0300

    Alterações no texto do post piloto.

--- posts/como-funciona-a-validacao-do-cpf/body.php
+++ posts/como-funciona-a-validacao-do-cpf/body.php
@@ -3,29 +3,32 @@
 </p>
 <h2 class="h3-size">Introdução</h2>
 <p>
-	Existem diversas situações em precisamos validar números de CPF para garantir a
-	integridade dos dados informados. Mas afinal, o que define se um número de CPF é
-	válido ou não? A seguir veremos as regras de validação do CPF e como aplicá-las a
-	uma função de validação escrita em PHP.
+	Existem diversas situações em que precisamos validar números de CPF para garantir a
+	integridade dos dados informados por usuários ou até mesmo aplicações de terceiros.
+	Mas afinal, o que define se um número de CPF é válido ou não? A seguir veremos as
+	regras de validação do CPF e como aplicá-las a uma função de validação escrita em PHP.
 </p>
 <h2 class="h3-size">Regra de validação do CPF</h2>
 <p>
-	O número de CPF segue o formato <code>999.999.999-99</code>: 11 dígitos sequenciais
+	Um número de CPF válido tem o formato <code>999.999.999-99</code>: 11 dígitos sequenciais
 	de 0 à 9, sendo os dois últimos utilizados como dígitos verificadores. Os separadores
 	(pontos e traço) servem apenas como máscara do número e são desprezados para fins
 	de validação.
 </p>
 <p>
-	Vamos tomar o CPF <code>111.222.333-45</code> como exemplo para as próximas etapas
-	da validação. Este CPF é composto por 11 dígitos de 0 à 9, portanto resta apenas
-	conferir se seus dígitos verificadores são de fato <strong>4</strong> e <strong>5</strong>.
+	Vamos tomar o número de CPF <code>111.222.333-45</code> como exemplo para ilustrar as
+	próximas etapas da validação. Este número de CPF é composto por 11 dígitos de 0 à 9,
+	portanto resta apenas conferir se seus dígitos verificadores são de fato <strong>4</strong>
+	e <strong>5</strong>. Para tanto, vamos calcular seus dígitos verificadores com base em
+	seus 9 primeiros dígitos.
 </p>
 <h2 class="h3-size">Cálculo do primeiro dígito verificador</h2>
 <p>
-	Para calcularmos o primero dígito verificador A de um CPF, devemos primeiramente
-	realizar o somatório das multiplicações de cada um dos <strong>9 primeiros dígitos</strong>
-	do CPF em questão por números <strong>decrescentes</strong> a partir de <strong>10</strong>.
-	Aplicando este cálculo ao nosso CPF de exemplo, temos:
+	Para calcularmos o primero dígito verificador A de um número de CPF, devemos primeiramente
+	multiplicar cada um de seus <strong>9 primeiros dígitos</strong>
+	por uma sequência de números <strong>decrescentes</strong> começando em <strong>10</strong>
+	e somar os resultados de todas estas multiplicações. Aplicando este cálculo ao nosso
+	número de CPF de exemplo, temos:
 </p>
 <pre>
 	<code>
@@ -35,21 +38,21 @@ A = 90
 	</code>
 </pre>
 <p>
-	Em seguida devemos realizar a <strong>divisão inteira</strong> do resultado obtido por
-	<strong>11</strong>, logo:
+	Em seguida devemos dividir o resultado obtido com este somatório por <strong>11</strong>,
+	logo:
 </p>
 <pre>
 	<code>
-A = A % 11
 A = 90 % 11
 A = 8 // Com resto 2
 	</code>
 </pre>
 <p>
-	A parte que nos interessa aqui é o <strong>resto</strong> desta divisão inteira. Se
-	o resto for <strong>menor do que 2</strong>, o primeiro dígito verificador será
-	<strong>0</strong>. Caso contrário, o primeiro dígito verificador será <strong>11 menos o resto</strong>.
-	Logo, o primeiro dígito verificador do nosso CPF de exemplo é <strong>9</strong>, pois:
+	A parte que nos interessa nesta divisão é o <strong>resto</strong> obtido. Se
+	este resto for <strong>menor do que 2</strong>, o primeiro dígito verificador será
+	<strong>0</strong>. Caso contrário, o primeiro dígito verificador será
+	<strong>11 menos o resto obtido</strong>. Logo, o primeiro dígito verificador do nosso
+	número de CPF de exemplo é <strong>9</strong>, pois:
 </p>
 <pre>
 	<code>
@@ -58,16 +61,17 @@ A = 9
 	</code>
 </pre>
 <p>
-	Neste ponto já sabemos que o nosso CPF de exemplo é composto pelos dígitos
-	<code>111.222.333-9?</code>. Resta apenas o último dígito.
+	Neste ponto já sabemos que o nosso número de CPF de exemplo é composto pelos dígitos
+	<code>111.222.333-9?</code>. Resta apenas calcularmos o segundo dígito verificador.
 </p>
 <h2 class="h3-size">Cálculo do segundo dígito verificador</h2>
 <p>
-	Para calcularmos o segundo dígito verificador B de um CPF, devemos primeiramente
-	realizar o somatório das multiplicações de cada um dos <strong>9 primeiros dígitos</strong>
-	do CPF em questão, <strong>mais o primeiro dígito verificador calculado anteriormente</strong>,
-	por números <strong>decrescentes</strong> a partir de <strong>11</strong>. Aplicando
-	este cálculo ao nosso CPF de exemplo, temos:
+	Para calcularmos o segundo dígito verificador B de um número de CPF, devemos primeiramente
+	multiplicar cada um de seus <strong>10 primeiros dígitos</strong> (sendo o décimo dígito o 
+	o primeiro dígito verificador calculado na etapa anterior) por uma sequência
+	de números <strong>decrescentes</strong> começando em <strong>11</strong> e somar
+	os resultados de todas estas multiplicações. Aplicando este cálculo ao nosso número de CPF de
+	exemplo, temos:
 </p>
 <pre>
 	<code>
@@ -77,8 +81,8 @@ B = 126
 	</code>
 </pre>
 <p>
-	Em seguida devemos realizar a <strong>divisão inteira</strong> do resultado
-	obtido por <strong>11</strong>, logo:
+	Em seguida devemos novamente dividir o resultado obtido com este somatório por
+	<strong>11</strong>, logo:
 </p>
 <pre>
 	<code>
@@ -88,11 +92,11 @@ B = 11 // Com resto 5
 	</code>
 </pre>
 <p>
-	Verificamos o <strong>resto</strong> da divisão da mesma forma como fizemos
-	no cálculo do primeiro dígito. Se o resto for <strong>menor do que 2</strong>,
-	o segundo dígito verificador será <strong>0</strong>. Caso contrário, o segundo
-	dígito verificador será <strong>11 menos o resto</strong>. Logo, o segundo dígito
-	verificador do nosso CPF de exemplo é <strong>6</strong>, pois:
+	Novamente avaliamos o <code>resto</code> obtido por esta divisão. Se este
+	resto for <strong>menor do que 2</strong>, o segundo dígito verificador será
+	<strong>0</strong>. Caso contrário, o segundo dígito verificador será
+	<strong>11 menos o resto obtido</strong>. Logo, o segundo dígito verificador do
+	nosso número de CPF de exemplo é <strong>6</strong>, pois:
 </p>
 <pre>
 	<code>
@@ -101,17 +105,18 @@ B = 6
 	</code>
 </pre>
 <p>
-	Pronto! Com isso concluímos que o nosso CPF de exemplo <code>111.222.333-45</code>
-	é <strong>inválido</strong>, pois seus dígitos verificadores são <strong>9</strong> e
-	<strong>6</strong>, formando o número de CPF <code>111.222.333-96</code>.
+	Pronto! Temos os dois dígitos verificadores do nosso número de CPF de exemplo.
+	A partir disso concluímos que o nosso CPF de exemplo <code>111.222.333-45</code> é
+	<strong>inválido</strong>, pois seus dígitos verificadores são <strong>9</strong> e
+	<strong>6</strong>, conforme calculamos e não 4 e 5.
 </p>
 <p>
-	Abaixo segue uma função escrita em PHP responsável por realizar os cálculos vistos
-	acima. A função retorna <code>true</code>, se o parâmetro passadp for um CPF válido,
-	ou <code>false</code>, caso contrário.
+	Abaixo segue uma função em PHP responsável por realizar todos os cálculos vistos
+	acima. A função retorna <code>true</code>, se o parâmetro passado for um número de
+	CPF válido (com ou sem os separadores), ou <code>false</code>, caso contrário.
 </p>
 <p>
-	<strong>ATENÇÃO</strong>: Números de CPF com todos os algarismos iguais, como
+	<strong>ATENÇÃO</strong>: Números de CPF com todos os dígitos iguais, como
 	por exemplo <code>111.111.111-11</code>, <code>222.222.222-22</code>, etc,
 	apesar de passarem pelos testes de validação, são considerados inválidos.
 </p>
@@ -153,11 +158,13 @@ function is_valid_cpf($cpf) {
 </pre>
 <h2 class="h3-size">Conclusão</h2>
 <p>
-	Conhecer as regras de validação do CPF pode ser bastante útil na hora de desenvolver extensões de bibliotecas 
-	de validação de dados, visto que o CPF é uma sequência numérica que representa um documento que só tem validade
-	no Brasil.
+	Conhecer as regras de validação de números de CPF pode ser bastante útil na
+	hora de desenvolver extensões de bibliotecas de validação de dados, visto que o
+	CPF é uma sequência numérica relevante apenas no Brasil.
 </p>
 <h2 class="h3-size">Saiba mais</h2>
 <ul id="related-links" class="block-list">
-	<li><a href="https://pt.wikipedia.org/wiki/D%C3%ADgito_verificador" target="_blank">Cálculo de dígito verificador</a></li>
+	<li><a href="https://pt.wikipedia.org/wiki/D%C3%ADgito_verificador" target="_blank">Dígito verificador</a></li>
+	<li><a href="http://www.somatematica.com.br/faq/cpf.php" target="_blank">Cálculo do dígito verificador do CPF</a></li>
+	<li><a href="https://www.geradordecpf.org/" target="_blank">Gerador de números de CPF</a></li>
 </ul>
--- views/home/presentation.php
+++ views/home/presentation.php
@@ -1,16 +1,17 @@
 <p>
 	Olá! Meu nome é Luccas Carvalho Silveira e eu sou desenvolvedor de software.
-	Atuo profissionalmente no desenvolvimento de sistemas baseados em PHP desde
+	Atuo profissionalmente como desenvolvedor de sistemas baseados em PHP desde
 	2010 e atualmente estou cursando o
 	<a href="http://www.pucrs.br/facin/curso/sistemas-de-informacao/" target="_blank">
-	bacharelado de sistemas de informação da PUCRS</a>.
+	bacharelado em sistemas de informação da PUCRS</a>.
 </p>
 <p>
-	Este espaço foi criado com o objetivo de compartilhar minhas experiências
-	na área de engenharia de software e web design, além de e dissertar sobre
-	tendências relevantes da área de tecnologia da informação.
+	Este espaço foi criado para que eu possa compartilhar minhas anotações e 
+	experiências nas áreas de engenharia de software e web design, além de
+	dissertar sobre tendências da área de tecnologia da informação que julgo
+	interessantes.
 </p>
 <p>
-	Sinta-se a vontade para explorar e compartilhar o conteúdo disponível neste
-	site.
+	Fique à vontade para explorar, editar e compartilhar o conteúdo disponível
+	neste site.
 </p>
