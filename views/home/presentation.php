1d1e869d7e7ba008f02e87fca44aabca7e764624
Luccas Silveira <luccas.silveira>
2017-12-11 17:49:32 -0200

    Continuação das alterações da tela inicial.

--- views/home/presentation.php
+++ views/home/presentation.php
@@ -1,14 +1,12 @@
 <p>
-	Olá! Meu nome é Luccas Carvalho Silveira e eu sou desenvolvedor de software.
-	Atuo profissionalmente como desenvolvedor de sistemas baseados em PHP desde
-	2010 e atualmente estou cursando o bacharelado em
+	Técnico em informática pelo IFSul em 2011 e atualmente graduando em
 	<a href="http://www.pucrs.br/facin/curso/sistemas-de-informacao/" target="_blank">
-	sistemas de informação</a> da PUCRS.
+	sistemas de informação</a> na PUCRS, venho desenvolvendo software profissionalmente
+	desde 2010.
 </p>
 <p>
-	Este espaço foi criado para que eu possa compartilhar minhas anotações e
-	experiências na área de engenharia de software e outros tópicos relacionados
-	à tecnologia da informação.
+	Este espaço foi criado para que eu possa compartilhar minhas observações e
+	experiências na área de engenharia de software.
 </p>
 <p>
 	Fique à vontade para explorar, editar e compartilhar o conteúdo disponível
