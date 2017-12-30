2cf8431ca4ccfb31015c576565efab4a8a5944c3
Luccas Silveira <luccas.carsil@gmail.com>
2017-12-30 21:18:04 -0200

    Correção da configuração de SMTP.

--- scripts/send_contact.php
+++ scripts/send_contact.php
@@ -1,119 +1,128 @@
 <?php
-	require './autoload.php';
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\Exception;
 
-	$name    = isset($_POST['name'])    ? $_POST['name']    : null;
-	$email   = isset($_POST['email'])   ? $_POST['email']   : null;
-	$subject = isset($_POST['subject']) ? $_POST['subject'] : null;
-	$message = isset($_POST['message']) ? $_POST['message'] : null;
+    require './autoload.php';
 
-	$form_fields = array(
-		array(
-			'name' => 'name',
-			'value' => $name,
-			'rules' => array(
-				array(
-					'rule' => 'notEmpty',
-					'message' => 'Campo de preenchimento obrigatório!'
-				)
-			)
-		),
-		array(
-			'name' => 'email',
-			'value' => $email,
-			'rules' => array(
-				array(
-					'rule' => 'notEmpty',
-					'message' => 'Campo de preenchimento obrigatório!'
-				),
-				array(
-					'rule' => 'isValidEmail',
-					'message' => 'Endereço de e-mail inválido!'
-				)
-			)
-		),
-		array(
-			'name' => 'subject',
-			'value' => $subject,
-			'rules' => array(
-				array(
-					'rule' => 'notEmpty',
-					'message' => 'Campo de preenchimento obrigatório!'
-				)
-			)
-		),
-		array(
-			'name' => 'message',
-			'value' => $message,
-			'rules' => array(
-				array(
-					'rule' => 'notEmpty',
-					'message' => 'Campo de preenchimento obrigatório!'
-				)
-			)
-		)
-	);
+    $name    = isset($_POST['name'])    ? $_POST['name']    : null;
+    $email   = isset($_POST['email'])   ? $_POST['email']   : null;
+    $subject = isset($_POST['subject']) ? $_POST['subject'] : null;
+    $message = isset($_POST['message']) ? $_POST['message'] : null;
 
-	$validator = new Validator();
-	$check = $validator->check($form_fields);
+    $form_fields = array(
+        array(
+            'name' => 'name',
+            'value' => $name,
+            'rules' => array(
+                array(
+                    'rule' => 'notEmpty',
+                    'message' => 'Campo de preenchimento obrigatório!'
+                )
+            )
+        ),
+        array(
+            'name' => 'email',
+            'value' => $email,
+            'rules' => array(
+                array(
+                    'rule' => 'notEmpty',
+                    'message' => 'Campo de preenchimento obrigatório!'
+                ),
+                array(
+                    'rule' => 'isValidEmail',
+                    'message' => 'Endereço de e-mail inválido!'
+                )
+            )
+        ),
+        array(
+            'name' => 'subject',
+            'value' => $subject,
+            'rules' => array(
+                array(
+                    'rule' => 'notEmpty',
+                    'message' => 'Campo de preenchimento obrigatório!'
+                )
+            )
+        ),
+        array(
+            'name' => 'message',
+            'value' => $message,
+            'rules' => array(
+                array(
+                    'rule' => 'notEmpty',
+                    'message' => 'Campo de preenchimento obrigatório!'
+                )
+            )
+        )
+    );
 
-	if ($check['success']) {
-		require './config.php';
+    $validator = new Validator();
+    $check = $validator->check($form_fields);
 
-		$smtp_config = Config::getSmtpConfig();
+    if ($check['success']) {
+        try {
+            require './config.php';
 
-		$phpMailer = new PHPMailer();
+            $smtp_config = Config::getSmtpConfig();
 
-		$phpMailer->CharSet = 'UTF-8';
-		$phpMailer->Encoding = 'base64';
+            $phpMailer = new PHPMailer();
 
-		//$phpMailer->SMTPDebug = 3;
-		$phpMailer->isSMTP();
-		$phpMailer->Host = $smtp_config['host'];
-		$phpMailer->SMTPAuth = true;
+            $phpMailer->CharSet = 'UTF-8';
+            $phpMailer->Encoding = 'base64';
 
-		$phpMailer->Username = $smtp_config['username'];
-		$phpMailer->Password = $smtp_config['password'];
-		$phpMailer->SMTPSecure = 'tls';
-		$phpMailer->Port = $smtp_config['port'];
+            //$phpMailer->SMTPDebug = 3;
 
-		$phpMailer->setFrom('contato@lu2cas.com.br', 'Contato [lu2cas.com.br]');
-		$phpMailer->addAddress('contato@lu2cas.com.br', 'Luccas Silveira');
+            $phpMailer->isSMTP();
+            $phpMailer->SMTPAuth = true;
+            $phpMailer->Host = $smtp_config['host'];
+            $phpMailer->Port = $smtp_config['port'];
+            $phpMailer->Username = $smtp_config['username'];
+            $phpMailer->Password = $smtp_config['password'];
 
-		$phpMailer->isHTML(true);
+            //$phpMailer->SMTPSecure = 'tls';
+            $phpMailer->SMTPAutoTLS = false;
 
-		$phpMailer->Subject = $subject;
+            $phpMailer->setFrom('contato@lu2cas.com.br', 'Contato [lu2cas.com.br]');
+            $phpMailer->addAddress('contato@lu2cas.com.br', 'Luccas Silveira');
 
-		$body = file_get_contents('./../views/emails/contact.html');
-		$body = str_replace(
-			array(
-				'{name}',
-				'{email}',
-				'{subject}',
-				'{message}'
-			),
-			array(
-				$name,
-				$email,
-				$subject,
-				$message
-			),
-			$body
-		);
-		$phpMailer->Body = $body;
+            $phpMailer->isHTML(true);
 
-		$send_email_success = $phpMailer->send();
-		$send_email_message = $send_email_success ? 'Mensagem enviada!' : 'Erro no envio!';
+            $phpMailer->Subject = $subject;
 
-		$return = array(
-			'is_valid_form' => true,
-			'send_email_success' => $send_email_success,
-			'send_email_message' => $send_email_message
-		);
-	} else {
-		$return = array(
-			'is_valid_form' => false,
-			'fields' => $check['fields']
-		);
-	}
+            $body = file_get_contents('./../views/emails/contact.html');
+            $body = str_replace(
+                array(
+                    '{name}',
+                    '{email}',
+                    '{subject}',
+                    '{message}'
+                ),
+                array(
+                    $name,
+                    $email,
+                    $subject,
+                    $message
+                ),
+                $body
+            );
+            $phpMailer->Body = $body;
 
-	die(json_encode($return));
+            $send_email_success = $phpMailer->send();
+            $send_email_message = $send_email_success ? 'Mensagem enviada!' : 'Erro no envio!';
+        } catch(Exception $e) {
+            $send_email_message = 'Erro no envio!';
+            $send_email_success = false;
+        }
+        $return = array(
+            'is_valid_form' => true,
+            'send_email_success' => $send_email_success,
+            'send_email_message' => $send_email_message
+        );
+    } else {
+        $return = array(
+            'is_valid_form' => false,
+            'fields' => $check['fields']
+        );
+    }
+
+    die(json_encode($return));
